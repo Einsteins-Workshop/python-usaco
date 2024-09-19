d0a03033441395a538917b369ae23d5b8f1eb9a3
dean <chung.dean@gmail.com>
2024-09-19 15:15:50 -0400

    Add Week 1 work

---
+++ euler_1.py
@@ -0,0 +1,5 @@
+str = "3 4"
+e=str.split()
+print(e[0])
+print(e[1])
+print(int(e[0])+int(e[1]))
---
+++ euler_3.py
@@ -0,0 +1,17 @@
+# See https://projecteuler.net/problem=3
+
+# The prime factors of 13195 are 5, 7, 13, and 29.
+
+# What is the largest prime factor of the number 600851475143
+
+# Replace the below with your program.
+print(29)
+datnumbah=600851475143
+def ibzatnumaprimethy(zat_number, primethy):
+    if zat_number % primethy == 0:
+        print("yass")
+        return(True)
+    else:
+        print("nopp")
+        return(False)
+ibzatnumaprimethy(datnumbah, 3)
