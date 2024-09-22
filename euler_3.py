ff58e7d145c75ddc6ec1f0b47b793c96164faf3d
Easton Zou <eastonzou@gmail.com>
2024-09-22 17:59:46 -0400

    I DID ITTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT

--- euler_3.py
+++ euler_3.py
@@ -5,10 +5,33 @@
 # What is the largest prime factor of the number 600851475143
 
 # Replace the below with your program.
-datnumbah=600851475143
-def ibzatnumaprimethy(zat_numbah, primethy):
-    if zat_numbah % primethy == 0:
-        return(True)
+import math
+BeeegNumber=600851475143
+BeeegNumber=int(BeeegNumber)
+    
+primer=[]
+possiblePrimes=[]
+for i in range(BeeegNumber):
+    possiblePrimes.append(i+1)
+possiblePrimes.remove(possiblePrimes[0])
+for a in range(len(possiblePrimes)):
+    if len(possiblePrimes)!=0:
+        primer.append(possiblePrimes[0])
+        for o in range(len(possiblePrimes)):
+            if o>=len(possiblePrimes):
+                break
+            if possiblePrimes[o]%primer[-1]==0:
+                possiblePrimes.remove(possiblePrimes[o])
     else:
-        return(False)
-ibzatnumaprimethy(datnumbah, 3)
+        break
+primeFactors=[]
+beeegestPrimeFactor=0
+print(primer)
+while len(primer)>0:
+    primethy=primer[0]
+    primer.remove(primer[0])
+    if BeeegNumber % primethy == 0:
+        primeFactors.append(primethy)
+        if primethy>beeegestPrimeFactor:
+            beeegestPrimeFactor=primethy
+print(beeegestPrimeFactor)
