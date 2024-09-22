1430618cae0332999c6c087d81c8174693c6def8
Easton Zou <eastonzou@gmail.com>
2024-09-22 19:34:45 -0400

    e

--- euler_4.py
+++ euler_4.py
@@ -6,20 +6,18 @@
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
 # Replace the below with your program.
-def isbobbyapalendrome(x):
+def isXAPalendrome(x):
     x=str(x)
     y=x [::-1]
     y=int(y)
     x=int(x)
     if x==y:
-        print("Yah, bobby is a palendrome")
         return True
     else:
-        print("Nuh, bobby aint no palendrome")
         return False
 z=0
 for x in range(100, 1000):
     for y in range(100, 1000):
-        if isbobbyapalendrome(x*y) and x*y>z:
+        if isXAPalendrome(x*y) and x*y>z:
             z=x*y
 print(z)
