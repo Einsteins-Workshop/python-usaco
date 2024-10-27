af0d2525883a06f63df1fa3b921327c1f98e99fe
Easton Zou <eastonzou@gmail.com>
2024-10-26 20:48:48 -0400

    w

--- project_euler/euler_15.py
+++ project_euler/euler_15.py
@@ -6,10 +6,11 @@
 # How many such routes are there through a 20x20 grid?
 
 # Replace the below with your program.
-def findRoutes(lefts, rights):
+def findRoutes(lefts, rights, mem={}):
+    if (lefts, rights) in mem:
+        return mem[(lefts, rights)]
     if lefts==0 or rights==0:
         return 1
-    return findRoutes(lefts-1, rights)+findRoutes(lefts, rights-1)
-inputa=input("A: ")
-inputb=input("B: ")
-print(findRoutes(int(inputa), int(inputb)))
+    mem[(lefts, rights)] = findRoutes(lefts-1, rights)+findRoutes(lefts, rights-1)
+    return mem[(lefts, rights)]
+print(findRoutes(20, 20))
