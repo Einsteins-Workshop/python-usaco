a07ccf8edf3262ce7f999b422b357d81049636bd
Easton Zou <eastonzou@gmail.com>
2024-11-12 10:22:24 -0500

    e

--- introduction/tic_tac_toe_simulator.py
+++ introduction/tic_tac_toe_simulator.py
@@ -16,22 +16,26 @@ def whoWon(board):
         return board[0]
     if whosSame(board[2], board[4], board[6])!=None:
         return board[2]
+
 def isWin(board):
     board = str(board)
     if isSame(board[0], board[1], board[2]) or isSame(board[3], board[4], board[5]) or isSame(board[6], board[7], board[8]) or isSame(board[0], board[3], board[6]) or isSame(board[1], board[4], board[7]) or isSame(board[2], board[5], board[8]) or isSame(board[0], board[4], board[8]) or isSame(board[2], board[4], board[6]):
         return True
     else:
         return False
+
 def isSame(a, b, c):
-    if a == b and b == c:
+    if a == b and b == c and a!="_":
         return True
     else:
         return False
+
 def whosSame(a, b, c):
-    if a == b and b == c:
+    if a == b and b == c and a!="_":
         return a
     else:
         return None
+
 def drawBoard(board):
     board = str(board)
     top_left = board[0]
@@ -45,7 +49,20 @@ def drawBoard(board):
     bottom_right = board[8]
     print(f"{top_left}{top_middle}{top_right}\n{middle_left}{middle_middle}{middle_right}\n{bottom_left}{bottom_middle}{bottom_right}")
 
-def findPossiblities(board, turn, depth):
-    pass
-drawBoard(123456789)
-print(isWin("123456789"))
+def findPossiblities(board, turn, turnsRemaining):
+    if isWin(board):
+        #return whoWon(board)
+        print(whoWon(board))
+        drawBoard(board)
+    board = str(board)
+    if turn=="x":
+        nextTurn="o"
+    else:
+        nextTurn="x"
+    for i in range(turnsRemaining):
+        newBoard=board
+        if newBoard[i]=="_":
+            newBoard=newBoard[:i]+turn+newBoard[i+1:]
+            findPossiblities(newBoard, nextTurn, turnsRemaining-1)
+
+findPossiblities("_________", "x", 9)
