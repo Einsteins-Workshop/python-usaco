2e50073a9eea4aebbbff6432c347a56317c3af5c
Easton Zou <eastonzou@gmail.com>
2024-11-17 16:41:36 -0500

    I FIXED IT

--- project_euler/euler_18.py
+++ project_euler/euler_18.py
@@ -31,7 +31,30 @@
 # Note: As there are only 16384 routes, it is possible to solve this problem by trying every route.
 # However, Problem 67 is the same challenge with a triangle containing one hundred rows; it cannot
 # be solved by brute force and requires a clever method!
-triangle=["75", "95", "64", "17", "47", "82", "18", "35", "87", "10", "20", "4", "82", "47", "65", "19", "1", "23", "75", "3", "34", "88", "2", "77", "73", "7", "63", "67", "99", "65", "4", "28", "6", "16", "70", "92", "41", "41", "26", "56", "83", "40", "80", "70", "33", "41", "48", "72", "33", "47", "32", "37", "16", "94", "29", "53", "71", "44", "65", "25", "43", "91", "52", "97", "51", "14", "70", "11", "33", "28", "77", "73", "17", "78", "39", "68", "17", "57", "91", "71", "52", "38", "17", "14", "91", "43", "58", "50", "27", "29", "48", "63", "66", "4", "68", "89", "53", "67", "30", "73", "16", "69", "87", "40", "31", "4", "62", "98", "27", "23", "9", "70", "98", "73", "93", "38", "53", "60", "4", "23"]
-def solve(lefts, rights, total):
-    if lefts==0 or rights==0:
-        pass
+
+triangle=[75, 95, 64, 17, 47, 82, 18, 35, 87, 10, 20, 4, 82, 47, 65, 19, 1, 23, 75, 3, 34, 88, 2, 77, 73, 7, 63, 67, 99, 65, 4, 28, 6, 16, 70, 92, 41, 41, 26, 56, 83, 40, 80, 70, 33, 41, 48, 72, 33, 47, 32, 37, 16, 94, 29, 53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, 70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57, 91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, 63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31, 4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]
+def sumation(x):
+    a=0
+    for i in range(x):
+        a=a+i+1
+    return a-x
+def getRow(rowNum):
+    sum=sumation(rowNum)
+    ans=triangle[sum:sum+rowNum]
+    return ans
+sums=[]
+def findSums(currRow, placesFromLeft, sum):
+    n=getRow(currRow)[placesFromLeft]
+    sum=sum+n
+    nextRow=getRow(currRow+1)
+    if nextRow==[]:
+        sums.append(sum)
+        return
+    findSums(currRow+1, placesFromLeft, sum)
+    findSums(currRow+1, placesFromLeft+1, sum)
+biggest=0
+findSums(1,0,0)
+for i in range(len(sums)):
+    if biggest<sums[i]:
+        biggest=sums[i]
+print(biggest)
