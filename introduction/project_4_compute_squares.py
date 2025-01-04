8b5d34869ab992cc24b89b1188962fee45d96ad5
dean <chung.dean@gmail.com>
2025-01-04 13:15:24 -0500

    Revert "Week 13 problems"
    This reverts commit 20b53a442293a0d696a6bd8f9f54c40830957ac5.

--- introduction/project_4_compute_squares.py
+++
@@ -1,7 +0,0 @@
-# Ask the user for a number
-number = int(input("Enter a number\n"))
-
-# Use a for loop and the range function to loop from 1 to number
-for i in range(0): # This needs to be completed.
-    pass # Remove this
-    # Print the square of the loop variable i.
--- introduction/project_5_fizz_buzz.py
+++
@@ -1,8 +0,0 @@
-# Ask for a number
-
-# Loop from 1 to that number
-    # For each number in that loop, print:
-    # FizzBuzz if the number is divisible by 3 and 5
-    # Fizz if the number is divisible by 3 but not 5
-    # Buzz if the number is divisible by 5 but not 3
-    # The number otherwise
