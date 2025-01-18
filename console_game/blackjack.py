2c0df0fac5999c3c730fe3e09985b12863ebbfd0
Easton <dean@einsteinsworkshop.com>
2025-01-18 15:27:09 -0500

    Week 14 class work

--- console_game/blackjack.py
+++ console_game/blackjack.py
@@ -1,77 +1,35 @@
-# First, create an object that represents a deck of cards
-class Deck:
-    def __init__(self):
-        # This represents all of the discarded cards
-        self._discard = []
-        # Create a list representing the deck of all cards not yet dealt out. This should be a set
-        # of values representing a card. The values could be a number from 0 to 51, a string with
-        # rank and suit, are a two element tuple with rank and suit. Make sure to save the deck to self.
-        pass
+import random
+def pickCard(alreadyPicked):
+    numbers=["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suites=["Spades", "Hearts", "Diamonds", "Clubs"]
+    attempt=" ".join([random.choice(numbers),"of",random.choice(suites)])
+    while attempt in alreadyPicked:
+        attempt = " ".join([random.choice(numbers), "of", random.choice(suites)])
+    return attempt
+def shuffle():
+    picks=[]
+    for i in range(52):
+        picks.append(pickCard(picks))
+    return picks
+def deal(amount):
+    deals=[]
+    for i in shuffledDeck:
+        amount=amount-1
+        deals.append(i)
+        shuffledDeck.remove(i)
+        if amount==0:
+            return deals
 
-    def deal_card(self):
-        # This function should randomly deal a card from the not already dealt cards and remove it
-        # from the deck.  It should return the dealt card.
-
-        # Also, determine whether the deck should be reshuffled. This could be when all cards are dealt,
-        # or when the cards get below some preset amount (like 10).
-        pass
-
-    def reshuffle(self):
-        # Reshuffle the deck by adding the discard pile to the undealt cards
-        pass
-
-    def reset(self):
-        # Set up the deck as it would be from the beginning, with all 52 cards
-        pass
-
-
-def card_name(value):
-    # Given a card value, return the card name so that it is easy to understand
-    pass
-
-def blackjack_value(value):
-    # Return the value of the card (1 for ace, 2-10 for other numbers, and 10 for Jack, Queen, and King
-    pass
-
-# Keep track of players and their current money
-
-# Loop until players quit
-    # Print out current players money totals
-    # Ask if players want another hand
-        # If players want to play, deal two cards to each player and to the house. Make sure that one
-        # of the house cards is marked as hidden, and the other as shown.
-
-        # Print out to all players their cards, and the revealed house card
-
-        # Loop among all players
-            # Ask (in a loop) if the player wants to hit (add another card)
-                # If the player wants to hit, deal a card.
-                    # If the players hand is over 21 (with aces evaluated as 1), declare that they bust
-                # If the player does not want to hit, stop that player's loop and go to the next player
-
-        # Reveal the house's hidden card
-
-        # If any player has not bust, do the dealer's play
-            # Deal (preferrably with a second of delay between cards) more cards to the dealer until
-            # the dealer's total is 17 or more, with aces counted as 11 if it brings the total between 17-21
-            # and 1 otherwise
-
-        # Resolve all hands. Two card blackjacks automatically win, player busts automatically lose, if
-        # the dealer busts, all non-busted players win, and otherwise anyone who has a higher total than
-        # the dealer wins, lower totals lose, and tied totals draw. Update all player money totals.
-
-# Other things to add:
-# 1. Player blackjack hands (starting with an Ace and a 10, J, Q, or K) should result in an automatic win
-# and 50% extra
-# 2. Allow players to doube down when their starting total is 9, 10, or 11. They get exactly one dealt card,
-# and win or lose double as normal
-# 3. Allow players insurance-- if the dealer has an ace, all players may opt for insurance.  Anyone can pay
-# half the bet for insurance. Then, check the dealer's hidden card. If it is 10, J, Q, or K, then anyone
-# who has paid for insurance wins a full bet (basically are even for the hand), and anyone without a
-# blackjack automatically loses. If the hidden card is not a 10, J, Q, or K, play continues as before and
-# anyone who paid for insurance pays the half bet.
-# 4. Splitting pairs-- if a player has a pair of the same denomination, they may split the cards into
-# two separate hands with a full separate bet. Each hand is resolved in the normal way unless the player
-# started with aces. Split aces only allow for a single card (for a total of two), and they do not get the
-# blackjack bonus if they hit an ace-ten.
-
+shuffledDeck=shuffle()
+for i in deal(2):
+    print(f"You got a {i}!")
+    if player==1:
+        player1cards.append[i]
+while True:
+    hitbool=input("Do you want to hit? (Yes/No): ")
+    if hitbool=="Yes":
+        print(f"You got a {deal(1)[0]}")
+    elif hitbool=="No":
+        break
+    else:
+        print("Please try again.")
--- console_game/mastermind.py
+++ console_game/mastermind.py
@@ -1,52 +1,25 @@
-# In this game, the computer will set up a multi-digit number, and the player will try to guess the
-# number. After each guess, the computer will reveal how many digits were exactly correct and how many
-# digits in the number but are not in the correct position.
-
-# Import the random module, see https://www.geeksforgeeks.org/python-random-function/ for more info.
-# Alternatively, you can use the "from random import randint" to just get random.
-
-# We define a constant
-DIGITS = 5
-class MastermindGame():
-    def __init__(self, digits):
-        # A parameter to check if a guess complets the game.
-        self._complete = False
-        # Create the hidden number, based on the number of digits
-
-    # Return True if the game is complete, False otherwise
-    def is_complete(self):
-        pass
-
-    def guess(self, guess):
-        # Check to make sure that the guess has the correct number of digits
-        correct = 0
-        incorrect = 0
-
-        # Check to see if the guess is exactly correct, and set the game to complete if the player
-        # guesses the hidden number
-
-        # Find how many digits in the guess are in the correct position. You can loop over all the digits
-        # to check
-
-        # Figure out how many digits in the guess are in the hidden number. You can do this by looping
-        # over all the digits from 0 - 9, see how many of those are in the hidden number, how many of those
-        # are in the guess, and take the lesser of these two quantities.
-
-        # The incorrect position numbers should be the different between the number of common digits in the
-        # previous step and the number of digits in the correct position
-
-        return correct, incorrect
-
-quit_playing = False
-current_game = None
-while True: # Change this loop so that we stop playing if the player quits
-    pass
-    # If there is no game or the game is complete, create a new game.
-
-    # Ask the player for a guess.
-
-    # Call the guess() function for the game and print out to the user the response of how many
-    # digits are in the right position and how many digits are in the hidden number but in the
-    # incorrect position.
-
-    # If the game is complete, ask the player if they want to play again
+import random
+def shuffle():
+    colors=["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
+    picks=[]
+    for i in range(len(colors)):
+        picked=random.choice(colors)
+        picks.append(picked)
+        colors.remove(picked)
+    return picks
+shuffled=shuffle()
+while True:
+    dapotsinside=input("Guess your colors(Red, Orange, Yellow, Green, Blue, Purple): ")
+    if str(dapotsinside)==str(shuffled):
+        print("You won!!! Yay!")
+    else:
+        potsarecool=dapotsinside.split(", ")
+        if len(potsarecool)==6 and "Red" in potsarecool and "Orange" in potsarecool and "Yellow" in potsarecool and "Green" in potsarecool and "Blue" in potsarecool and "Purple" in potsarecool:
+            truedat=False
+            print("No, but,")
+            for i in range(len(shuffled)):
+                if potsarecool[i] == shuffled[i]:
+                    print(f"Number {i + 1} is correct.")
+                    truedat=True
+            if truedat==False:
+                print("Everything is wrong")
