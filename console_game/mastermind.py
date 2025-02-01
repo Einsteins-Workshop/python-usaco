9c60c460764ba192eb5a063f9fa544af337697ed
Easton <dean@einsteinsworkshop.com>
2025-02-01 15:21:01 -0500

    something

--- console_game/mastermind.py
+++ console_game/mastermind.py
@@ -1,33 +1,54 @@
 import random
+choice1=str("R")
+choice2=str("O")
+choice3=str("Y")
+choice4=str("G")
+choice5=str("B")
+choice6=str("P")
+
+# How to play this game:
+# Hit start, or run the command
+# python mastermind.py
+# in console.
+
+# How to use different options:
+# Change the variables.
+# Example:
+# choice1=str("R") --> choice1=str(1)
+
+# How to add a new option:
+# Ctrl+F to find all instances of "choice6"
+# Following the syntax of the stuff around "choice6", add "choice7" behind it.
+# Add a new variable named "choice7" and set it to str(Whatever you want here)
 def shuffle():
-    colors=["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
+    choices=[choice1, choice2, choice3, choice4, choice5, choice6]
     picks=[]
-    for i in range(len(colors)):
-        picked=random.choice(colors)
+    for i in range(len(choices)):
+        picked=random.choice(choices)
         picks.append(picked)
-        colors.remove(picked)
+        choices.remove(picked)
     return picks
 score=0
 while True:
     shuffled=shuffle()
     while True:
-        dapotsinside=input("Guess your colors(Red, Orange, Yellow, Green, Blue, Purple). \nMake sure to use this syntax: Red, Orange, Yellow, Green, Blue, Purple: ")
-        if dapotsinside.split(", ")==shuffled:
+        userGuess=input(f"Pick your choices out of {choice1}, {choice2}, {choice3}, {choice4}, {choice5}, {choice6}. \nMake sure to use this exact syntax: {choice1}, {choice2}, {choice3}, {choice4}, {choice5}, {choice6} \nNow, what is your guess?: ").split(", ")
+        if userGuess==shuffled:
             break
         else:
-            potsarecool=dapotsinside.split(", ")
-            if len(potsarecool)==6 and "Red" in potsarecool and "Orange" in potsarecool and "Yellow" in potsarecool and "Green" in potsarecool and "Blue" in potsarecool and "Purple" in potsarecool:
+            if len(userGuess)==6 and choice1 in userGuess and choice2 in userGuess and choice3 in userGuess and choice4 in userGuess and choice5 in userGuess and choice6 in userGuess:
                 truedat=False
                 print("No, but,")
                 score=score-1
                 for i in range(len(shuffled)):
-                    if potsarecool[i] == shuffled[i]:
+                    if userGuess[i] == shuffled[i]:
                         print(f"Number {i + 1} is correct.")
                         truedat=True
                 if truedat==False:
                     print("Everything is wrong")
     print("You won!!! Yay!")
     score=score+10
-    if input("Play again?(Yes/No): ")=="No":
+    print(f"Your score is {score}.")
+    if input("Would you like to play again?(Yes/No): ")=="No":
         break
 print(f"Goodbye. Your final score was: {score}.")
