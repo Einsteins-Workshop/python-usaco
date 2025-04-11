9fc20a6794c4baca24ab9e2f684f7aba4cc41efd
Easton Zou <eastonzou@gmail.com>
2025-04-10 20:00:18 -0400

    i made it into a folder im happy

--- machine_learning/Chat AI Thing/Chat AI Thing (CAT).py
+++ machine_learning/Chat AI Thing/Chat AI Thing (CAT).py
@@ -1,10 +1,14 @@
 import json
 import os
 print("CAT: Hello! I am CAT. CAT stands for Chat AI Thing. ")
-name=input("CAT: What is you name? \nYour name: ")
-if os.path.exists(f"{name}'s file"):
+name=input("CAT: What is your name? \nYour name: ")
+folder="catUserFiles"
+if not os.path.exists(folder):
+    os.makedirs(folder)
+filePath=os.path.join(folder,f"{name}'s file")
+if os.path.exists(filePath):
     print(f"CAT: Nice to see you again {name}. ")
-    with open(f"{name}'s file") as fileHandle:
+    with open(filePath) as fileHandle:
         userInputs=json.load(fileHandle)
 else:
     userInputs={}
@@ -18,5 +22,5 @@ while True:
     else:
         correctOutput=input("CAT: I haven't heard that from you before! What would be appropriate to say here? \nAppropriate response: ")
         userInputs[userInput]=correctOutput
-        with open(f"{name}'s file","w") as fileHandle:
+        with open(filePath,"w") as fileHandle:
             json.dump(userInputs,fileHandle)
