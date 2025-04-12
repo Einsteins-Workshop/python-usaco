cc49ff5ffbf8c901be5d3831e1d72c513431c975
Easton <dean@einsteinsworkshop.com>
2025-04-12 15:25:43 -0400

    Week 30 changes

--- machine_learning/Chat AI Thing/Chat AI Thing (CAT).py
+++ machine_learning/Chat AI Thing/Chat AI Thing (CAT).py
@@ -2,10 +2,10 @@ import json
 import os
 print("CAT: Hello! I am CAT. CAT stands for Chat AI Thing. ")
 name=input("CAT: What is your name? \nYour name: ")
-folder="catUserFiles"
+folder="userCATdata"
 if not os.path.exists(folder):
     os.makedirs(folder)
-filePath=os.path.join(folder,f"{name}'s file")
+filePath=os.path.join(folder,f"{name}'s CAT data.json")
 if os.path.exists(filePath):
     print(f"CAT: Nice to see you again {name}. ")
     with open(filePath) as fileHandle:
@@ -13,6 +13,19 @@ if os.path.exists(filePath):
 else:
     userInputs={}
     print(f"CAT: Nice to meet you {name}. I'm still learning so please be patient with me. ")
+files=[f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder,f))]
+
+listOfDictionariesOfCATuserFiles=[]
+#for file in files:
+#    with open(os.path.join(folder,file)) as userFile:
+#        listOfDictionariesOfCATuserFiles.append(json.load(userFile))
+#userVotes={}
+#for CATuserFile in listOfDictionariesOfCATuserFiles:
+#    for key, value in CATuserFile.items():
+#        if userVotes[key]>=0:
+#            userVotes[key]=userVotes[key]+1
+#        else:
+#            userVotes[key]=0
 x=0
 while True:
     x=x+1
